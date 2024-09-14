d1ff8293ae2e02d1bb6c1128d505abc171909e91
vidyasagar0405 <vidyasagar0405@gmail.com>
2024-09-14 20:00:28 +0530

    formatting

--- modules/Help.py
+++ modules/Help.py
@@ -2,16 +2,19 @@ from pathlib import Path
 from textual.widgets import Markdown
 from textual.app import ComposeResult
 
+
 # Function to read the Markdown file
 def read_markdown_file(file_path: str) -> str:
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         return file.read()
 
+
 # Specify the path to the Help.md file
-help_markdown_path = Path('modules/Help.md')
+help_markdown_path = Path("modules/Help.md")
 
 # Read the content of the Markdown file
 help_markdown_content = read_markdown_file(help_markdown_path)
+
 
 class HelpMarkdown(Markdown):
     def compose(self) -> ComposeResult:
--- modules/logging.py
+++ modules/logging.py
@@ -1,8 +1,9 @@
 import logging
 
+
 def setup_logging():
     logging.basicConfig(
-        filename='master_log.log',
+        filename="master_log.log",
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
+        format="%(asctime)s - %(levelname)s - %(message)s",
     )
