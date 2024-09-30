5fa0bdd91e5ece79ec61bce96f033060ad61f4b9
vidyasagar0405 <vidyasagar0405@gmail.com>
2024-09-30 19:36:18 +0530

    up

--- modules/Pipeline_widgets.py
+++ modules/Pipeline_widgets.py
@@ -1,19 +1,18 @@
 from textual.app import ComposeResult, on
 from textual.widgets import Button, Label, Static, Input
-
+from textual.containers import Vertical
 
 class PipelineWidgets(Static):
     def compose(self) -> ComposeResult:
-        yield Label(
-            "It is recommended to do fastqc and make sure the reads are of sufficiant quality before starting the pipeline"
-        )
+        with Vertical(id="Pipeline_container"):
+            yield Label( "[bold red]NOTE: [/bold red]It is recommended to do fastqc and make sure the reads are of sufficiant quality before starting the pipeline")
 
-        yield Input(placeholder="Enter project name", id="pipeline_project_name_input")
-        yield Input(placeholder="Enter reference file", id="pipeline_ref_input")
-        yield Input(placeholder="Enter a read file", id="pipeline_read1_input")
-        yield Input(placeholder="Enter the other read file", id="pipeline_read2_input")
+            yield Input(placeholder="Enter project name", id="pipeline_project_name_input")
+            yield Input(placeholder="Enter reference file", id="pipeline_ref_input")
+            yield Input(placeholder="Enter a read file", id="pipeline_read1_input")
+            yield Input(placeholder="Enter the other read file", id="pipeline_read2_input")
 
-        yield Button("Start Pipeline", id="start_pipeline_button")
+            yield Button("Start Pipeline", id="start_pipeline_button", variant="success")
 
     @on(Button.Pressed, "#start_pipeline_button")
     def start_pipeline(self):
