95c5def08ef2702f960a2f058b048559fab3dddc
vidyasagar0405 <vidyasagar0405@gmail.com>
2024-09-30 15:55:13 +0530

    up

--- modules/Pipeline_widgets.py
+++ modules/Pipeline_widgets.py
@@ -1,7 +1,14 @@
 from textual.app import ComposeResult
-from textual.widgets import Button, Static
+from textual.widgets import Button, Label, Static, Input
 
 
 class PipelineWidgets(Static):
     def compose(self) -> ComposeResult:
-        yield Button("Pipeline")
+        yield Label(
+            "It is recommended to do fastqc and make sure the reads are of sufficiant quality before starting the pipeline"
+        )
+        yield Input(placeholder="Enter project name", id="pipeline_project_name_input")
+        yield Input(placeholder="Enter reference file", id="pipeline_ref_input")
+        yield Input(placeholder="Enter a read file", id="pipeline_read1_input")
+        yield Input(placeholder="Enter the other read file", id="pipeline_read2_input")
+        yield Button("Start Pipeline")
