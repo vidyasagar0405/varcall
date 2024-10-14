ea18b52ab447bfc02cf24c05839e8bb4eb503422
vidyasagar0405 <vidyasagar0405@gmail.com>
2024-10-14 19:05:18 +0530

    added all bcftool widgets

--- modules/Bcftools_widgets.py
+++ modules/Bcftools_widgets.py
@@ -1,13 +1,54 @@
 from textual.app import ComposeResult
-from textual.widgets import Button, Static
+from textual.containers import Container, Horizontal
+from textual.widgets import Button, Static, Label, Input
+
+from modules.exec_func.common_funcs import file_suggester, get_input
 
 
 class BcftoolsWidgets(Static):
     def compose(self) -> ComposeResult:
-        yield Button()
-        yield Button()
-        yield Button()
 
+        with Container(id="bam_mpileup_container"):
+            yield Label("bamtools mpileup")
+            with Horizontal(id="bam_mpileup_horizontal"):
+                yield Input( placeholder="reference", id="bam_mpileup_reference_input", suggester=file_suggester,)
+                yield Input( placeholder="input bam file name", id="bam_mpileup_input_input", suggester=file_suggester,)
+                yield Input( placeholder="output bam file name", id="bam_mpileup_output_input", suggester=file_suggester,)
+                yield Button( "mpileup", id="bam_mpileup_button",)
+            yield Label("[bold #01B0DC]Description: [/bold #01B0DC]Convert .bam files to .bam files. you can also select only to convert a certain region", classes="description")
+
+        with Container(id="bam_call_container"):
+            yield Label("bamtools call")
+            with Horizontal(id="bam_call_horizontal"):
+                yield Input( placeholder="input bam/bam file name", id="bam_call_input_input", suggester=file_suggester,)
+                yield Input( placeholder="output bam/bam file name", id="bam_call_output_input", suggester=file_suggester,)
+                yield Button("call bam file", id="bam_call_button")
+            yield Label("[bold #01B0DC]Description: [/bold #01B0DC].bam and .bam files need to be called before futher processing", classes="description")
+
+        with Container(id="bam_filter_container"):
+            yield Label("bamtools filter")
+            with Horizontal(id="bam_filter_horizontal"):
+                yield Input( placeholder="filter", id="bam_filter_filter_input", suggester=file_suggester,)
+                yield Input( placeholder="input bam file name", id="bam_filter_input_input", suggester=file_suggester,)
+                yield Input( placeholder="output bam file name", id="bam_filter_output_input", suggester=file_suggester,)
+                yield Button( "filter", id="bam_filter_button",)
+            yield Label("[bold #01B0DC]Description: [/bold #01B0DC]Convert .bam files to .bam files. you can also select only to convert a certain region", classes="description")
+
+        with Container(id="bam_norm_container"):
+            yield Label("bamtools norm")
+            with Horizontal(id="bam_norm_horizontal"):
+                yield Input( placeholder="input bam/bam file name", id="bam_norm_input_input", suggester=file_suggester,)
+                yield Input( placeholder="output bam/bam file name", id="bam_norm_output_input", suggester=file_suggester,)
+                yield Button("norm bam file", id="bam_call_button")
+            yield Label("[bold #01B0DC]Description: [/bold #01B0DC].bam and .bam files need to be normed before futher processing", classes="description")
+
+        with Container(id="bam_stats_container"):
+            yield Label("bamtools stats")
+            with Horizontal(id="bam_stats_horizontal"):
+                yield Input( placeholder="input bam/bam file name", id="bam_stats_input_input", suggester=file_suggester,)
+                yield Input( placeholder="output bam/bam file name", id="bam_stats_output_input", suggester=file_suggester,)
+                yield Button("stats bam file", id="bam_stats_button")
+            yield Label("[bold #01B0DC]Description: [/bold #01B0DC].bam and .bam files need to be statsed before futher processing", classes="description")
 
 # TODO:
 
--- modules/exec_func/bcftools_funcs.py
+++ modules/exec_func/bcftools_funcs.py
@@ -1,51 +1,3 @@
-# import subprocess
-# import threading
-#
-#
-# # Function to run a command using subprocess and threading for parallel execution
-# def run_command(cmd):
-#     process = subprocess.Popen(
-#         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-#     )
-#     stdout, stderr = process.communicate()
-#     if process.returncode != 0:
-#         print(f"Error running command: {cmd}\n{stderr.decode()}")
-#     else:
-#         print(stdout.decode())
-#
-#
-# # 1. bcftools mpileup command
-# def bcftools_mpileup(bam_file, reference, output_file):
-#     cmd = f"bcftools mpileup -f {reference} {bam_file} -o {output_file}"
-#     run_command(cmd)
-#
-#
-# # 2. bcftools call command
-# def bcftools_call(input_file, output_file):
-#     cmd = f"bcftools call -mv -Oz -o {output_file} {input_file}"
-#     run_command(cmd)
-#
-#
-# # 3. bcftools filter command
-# def bcftools_filter(input_file, output_file, filter_expression):
-#     cmd = f"bcftools filter -s LOWQUAL -e '{filter_expression}' {input_file} -o {output_file}"
-#     run_command(cmd)
-#
-#
-# # 4. bcftools norm command
-# def bcftools_norm(input_file, output_file):
-#     cmd = f"bcftools norm -f {input_file} {output_file}"
-#     run_command(cmd)
-#
-#
-# # 5. bcftools stats command
-# def bcftools_stats(input_file, output_file):
-#     cmd = f"bcftools stats {input_file} > {output_file}"
-#     run_command(cmd)
-#
-#
-#
-#
 import subprocess
 import threading
 
@@ -171,23 +123,3 @@ def bcftools_stats(input_file, output_file):
     cmd = f"bcftools stats {input_file} > {output_file}"
     # Run the command using subprocess to generate the stats
     run_command(cmd)
-
-
-# Example of running commands in parallel
-# if __name__ == "__main__":
-#     # Define some example inputs
-#     bam_file = "example.bam"
-#     reference = "reference.fasta"
-#     output_mpileup = "output_mpileup.bcf"
-#     output_call = "output_call.vcf.gz"
-#     filter_expression = "QUAL<20"
-#
-#     # Run commands in parallel threads
-#     mpileup_thread = bcftools_mpileup(bam_file, reference, output_mpileup)
-#     call_thread = bcftools_call(output_mpileup, output_call)
-#     filter_thread = bcftools_filter(output_call, "filtered_output.vcf.gz", filter_expression)
-#
-#     # Wait for all threads to finish
-#     mpileup_thread.join()
-#     call_thread.join()
-#     filter_thread.join()
